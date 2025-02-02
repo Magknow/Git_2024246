9c42f88a420d9338327a83e06e1c301d79aa25ba
Magknow <magnunmariano@gmail.com>
2025-02-02 21:18:10 +0100

    Added previous homework code

--- src/git_2024246/Git_2024246.java
+++ src/git_2024246/Git_2024246.java
@@ -4,6 +4,8 @@
  */
 package git_2024246;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Magnow
@@ -16,11 +18,37 @@ public class Git_2024246 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        //Outputting the name and the student ID
-        System.out.println("Magnun Mariano - 2024246");
+            //Outputting the name and the student ID
+            System.out.println("Magnun Mariano - 2024246");
         
+        // Creating a Scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
         
+        // Declaring an integer variable to store the user's input
+        int myKb;  
         
+        // Using while loop that runs until an even number is entered
+        while (true) {  
+            // Prompt user to input
+            System.out.print("Enter an even number: ");  
+            // Read user input and store it in 'num'
+            myKb = scanner.nextInt();  
+
+            // Checking if the number is even
+            if (myKb % 2 == 0) {  
+                // outputing confirmation message
+                System.out.println("Thank you! number " + myKb + " is even.");  
+                
+                // Exit the loop since the condition is met
+                break; 
+            
+            // If the number is not even
+            } else {  
+                System.out.println("Sorry," + myKb + " is not even. Please, try again."); 
+            }
+        }
+
+        scanner.close();
     }
     
 }
